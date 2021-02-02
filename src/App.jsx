06029961c76399145bf39c6faf2faa58389536cc
ElioRoman <kolomiets.roman30@gmail.com>
2021-02-02 21:49:48 +0200

    add file

--- src/App.jsx
+++ src/App.jsx
@@ -7,20 +7,46 @@ import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
 import './common.scss';
 
 class App extends Component {
+  state = {
+    weekStartDate: new Date(),
+  };
 
-    state = {
-        weekStartDate: new Date(),
-    }
+  handleTodayWeek = () => {
+    this.setState({
+      weekStartDate: new Date(),
+    });
+  };
 
-    render() {
-        const { weekStartDate } = this.state;
-        const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+  handleNextWeek = () => {
+    const newDate = this.state.weekStartDate;
+    this.setState({
+      weekStartDate: new Date(newDate.setDate(newDate.getDate() + 7)),
+    });
+  };
 
-        return (<>
-            <Header />
-            <Calendar weekDates={weekDates} />
-        </>)
-    }
-};
+  handlePrevWeek = () => {
+    const newDate = this.state.weekStartDate;
+    this.setState({
+      weekStartDate: new Date(newDate.setDate(newDate.getDate() - 7)),
+    });
+  };
+
+  render() {
+    const { weekStartDate } = this.state;
+    const weekDates = generateWeekRange(getWeekStartDate(weekStartDate));
+
+    return (
+      <>
+        <Header
+          todayWeek={this.handleTodayWeek}
+          prevWeek={this.handlePrevWeek}
+          nextWeek={this.handleNextWeek}
+          weekDates={weekDates}
+        />
+        <Calendar weekDates={weekDates} />
+      </>
+    );
+  }
+}
 
 export default App;
--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,29 +1,35 @@
 import React from 'react';
+import moment from 'moment';
 
 import { months } from '../../utils/dateUtils.js';
 
 import './header.scss';
 
-const Header = () => {
-    return (
-        <header className="header">
-            <button className="button create-event-btn">
-                <i className="fas fa-plus create-event-btn__icon"></i>Create
-            </button>
-            <div className="navigation">
-                <button className="navigation__today-btn button">
-                    Today
-                </button>
-                <button className="icon-button navigation__nav-icon">
-                    <i className="fas fa-chevron-left"></i>
-                </button>
-                <button className="icon-button navigation__nav-icon">
-                    <i className="fas fa-chevron-right"></i>
-                </button>
-                <span className="navigation__displayed-month"></span>
-            </div>
-        </header>
-    )
-}
+const Header = ({ todayWeek, prevWeek, nextWeek, weekDates }) => {
+  const firstWeekday = moment(weekDates[0]).format('MMMM Y');
+  const lastWeekday = moment(weekDates[weekDates.length - 1]).format('MMMM Y');
+  const monthName =
+    firstWeekday === lastWeekday ? firstWeekday : `${firstWeekday} - ${lastWeekday}`;
+
+  return (
+    <header className="header">
+      <button className="button create-event-btn">
+        <i className="fas fa-plus create-event-btn__icon"></i>Create
+      </button>
+      <div className="navigation">
+        <button className="navigation__today-btn button" onClick={todayWeek}>
+          Today
+        </button>
+        <button className="icon-button navigation__nav-icon" onClick={prevWeek}>
+          <i className="fas fa-chevron-left"></i>
+        </button>
+        <button className="icon-button navigation__nav-icon" onClick={nextWeek}>
+          <i className="fas fa-chevron-right"></i>
+        </button>
+        <span className="navigation__displayed-month">{monthName}</span>
+      </div>
+    </header>
+  );
+};
 
 export default Header;
