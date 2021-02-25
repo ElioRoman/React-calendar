e1570c08921705bee619851842314f41f1d6d548
Roman <kolomiets.roman30@gmail.com>
2021-02-25 12:38:25 +0200

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,7 @@
 import React, { useState } from 'react';
 import Header from './components/header/Header.jsx';
 import Calendar from './components/calendar/Calendar.jsx';
-
 import { getWeekStartDate, generateWeekRange } from '../src/utils/dateUtils.js';
-
 import './common.scss';
 
 const App = () => {
