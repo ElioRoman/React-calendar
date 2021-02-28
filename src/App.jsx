64d086c2e1e441d5cf2aec53448c335e36def839
Roman <kolomiets.roman30@gmail.com>
2021-02-28 18:25:49 +0200

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -7,39 +7,28 @@ import './common.scss';
 const App = () => {
   const [startDateWeek, setStartDateWeek] = useState(new Date());
   const [isOpenModalWindow, setIsOpenModalWindow] = useState(false);
-
-  const handleNextWeek = () => {
-    setStartDateWeek(new Date(startDateWeek.setDate(startDateWeek.getDate() + 7)));
-  };
-
-  const handlePrevWeek = () => {
-    setStartDateWeek(new Date(startDateWeek.setDate(startDateWeek.getDate() - 7)));
-  };
-
-  const handleTodayWeek = () => {
-    setStartDateWeek(new Date());
-  };
-
-  const handleOpenModal = () => {
-    setIsOpenModalWindow(true);
-  };
-
-  const handleCloseModal = () => {
-    setIsOpenModalWindow(false);
-  };
-
   const weekDates = generateWeekRange(getWeekStartDate(startDateWeek));
 
   return (
     <>
       <Header
-        nextWeek={handleNextWeek}
-        prevWeek={handlePrevWeek}
-        todayWeek={handleTodayWeek}
+        nextWeek={() => {
+    setStartDateWeek(new Date(startDateWeek.setDate(startDateWeek.getDate() + 7)));
+  }}
+        prevWeek={() => {
+    setStartDateWeek(new Date(startDateWeek.setDate(startDateWeek.getDate() - 7)));
+  }}
+        todayWeek={() => {
+    setStartDateWeek(new Date());
+  }}
         weekDates={weekDates}
-        openModal={handleOpenModal}
+        openModal={() => {
+    setIsOpenModalWindow(true);
+  }}
       />
-      <Calendar weekDates={weekDates} openModal={isOpenModalWindow} closeModal={handleCloseModal} />
+      <Calendar weekDates={weekDates} openModal={isOpenModalWindow} closeModal={() => {
+    setIsOpenModalWindow(false);
+  }} />
     </>
   );
 };
